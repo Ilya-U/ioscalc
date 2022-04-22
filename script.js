62f5ecdab5a689325c0330030c8e815aa0f3e81d
Ilya Ufimtsev <91266234+Ilya-U@users.noreply.github.com>
2022-04-23 00:36:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function show(arg)
+{
+    if (document.getElementById("screen").innerHTML == 0 || document.getElementById("screen").innerHTML == "Error" || document.getElementById("screen").innerHTML == "NaN")
+    {
+        document.getElementById("screen").innerHTML = arg;
+        return;
+    }
+    document.getElementById("screen").innerHTML += arg;
+}
+
+function result()
+{
+    ans = eval(document.getElementById("screen").innerHTML);
+    if (ans == Infinity || ans == -Infinity)
+    {
+        document.getElementById("screen").innerHTML = "Error";
+        return;
+    }
+    document.getElementById("screen").innerHTML = ans;
+}
+
+function Clear()
+{
+    document.getElementById("screen").innerHTML = "0";
+}
+
+function minus()
+{
+    document.getElementById("screen").innerHTML = -(document.getElementById("screen").innerHTML);
+}
+
+function percent()
+{
+    document.getElementById("screen").innerHTML = document.getElementById("screen").innerHTML / 100
+}
